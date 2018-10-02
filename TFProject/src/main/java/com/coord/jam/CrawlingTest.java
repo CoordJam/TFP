1f2bc4bbba8cb6afcea072cd1bc8adf25c97430f
orange <orange@DESKTOP-3TCTIDQ>
2018-10-02 17:51:31 +0900

    dd

--- TFProject/src/main/java/com/coord/jam/CrawlingTest.java
+++ TFProject/src/main/java/com/coord/jam/CrawlingTest.java
@@ -6,46 +6,37 @@ import java.util.List;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class CrawlingTest {
-
 	public List<String> designerName(){
 		List<String> list=new ArrayList<String>();
-		//int CountryCount;
 		try {
-			for(int i=1;i<114;i++) {
-			String URL = "https://en.wikipedia.org/w/index.php?title=List_of_fashion_designers&action=edit&section="+i;
+			String URL = "https://en.wikipedia.org/w/index.php?title=List_of_fashion_designers";
+			int n=0;
 			Document doc = Jsoup.connect(URL).get();
-			Elements el=doc.select("#wpTextbox1");
-			list.add(el.text());
-			//System.out.println(list);
+			Elements elsCountry=doc.select(".mw-headline");
+			Elements elsName=doc.select("ul:not(.toc)");
+			for(Element country:elsCountry) {
+				System.out.println(country.text());
+				Element name=elsName.get(n);
+				System.out.println(name.text());
+				/*list.add(country.text()+name.text());
+				
+				System.out.println(list);*/
+				n++;
 			}
-			/*int n=1;
-			Elements Country=doc.select("h2");
-			for(Element el:Country.select(".mw-headline")) {
-			//System.out.println(el.text());
-			list.add(el.text());
-			list.add(Integer.toString(n));
-			n++;
-			}
-			list.remove(list.size()-1);
-			System.out.println(list);
-			CountryCount=list.size();*/
 			
-			/*Elements Country2=doc.select("#mw-content-text > div > div:nth-child(5)");
-			for(Element el:Country2.select(" ul > li")){
-				System.out.println(el.text());
-			}*/
-			
+
 		}catch(IOException e){
 			e.printStackTrace();
 		}
 
 		return list;
 	}
-	/*public static void main(String[] args) {
+	public static void main(String[] args) {
 		CrawlingTest crawl=new CrawlingTest();
 		crawl.designerName();
-	}*/
+	}
 }
